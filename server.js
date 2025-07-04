16ec4a53be888e7ae7814f2853504a12b9bfb27c
ypradeeprao <39065049+ypradeeprao@users.noreply.github.com>
2025-07-04 08:16:02 -0500

    Update server.js

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.post('/weather', (req, res) => {
 
   // Simulated weather result
   const response = {
-    response: `☀️ It is sunny today in ${city}.`,
+    response: `☀️ It is sunnysun today in ${city}.`,
     metadata: {
       city,
       forecast: "sunny",
